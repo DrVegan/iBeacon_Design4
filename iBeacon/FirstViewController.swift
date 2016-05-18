4e705945c852267c380179cf86a245b86d0e09f0
Daniel Freeman <dr.freeman@me.com>
2016-05-18 21:56:54 +1000

    Initial Commit

---
+++ iBeacon/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  iBeacon
+//
+//  Created by Daniel Freeman on 18/05/2016.
+//  Copyright © 2016 Daniel Freeman. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ iBeacon/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  iBeacon
+//
+//  Created by Daniel Freeman on 18/05/2016.
+//  Copyright © 2016 Daniel Freeman. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
